36fbd478345cd56048871c1babf6ea7421170c95
mehrdadkl7 <mehrdadhooshanginaghani@gmail.com>
2017-08-04 15:45:48 +0430

    Initial Commit

---
+++ Who's The Killer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Who's The Killer
+//
+//  Created by mehrdad hooshangi on 5/13/1396 AP.
+//  Copyright © 1396 mehrdad hooshangi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
